8d6b85aa006232e640040858ad2187ba906ed456
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-05-25 19:12:22 +0900

    投稿機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,19 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    rederct_to '/'
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+  
 end

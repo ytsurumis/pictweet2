01802e196ad7bc9f089e2e7a62f70e9fc5f0605d
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-05-27 06:25:58 +0900

    詳細表示の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+
   def index
     @tweets = Tweet.all
   end
@@ -9,12 +11,34 @@ class TweetsController < ApplicationController
 
   def create
     Tweet.create(tweet_params)
-    rederct_to '/'
+    redirect_to root_path
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
+  def show
   end
 
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
   end
-  
+
+  def set_tweet
+    @tweet = Tweet.find(prarams[:id])
+  end
+
 end

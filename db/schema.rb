9e857cb43a8bb98c4259a7e22250315527135e89
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-06-01 14:32:59 +0900

    user_idカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_26_215216) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_28_213237) do
   create_table "tweets", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
